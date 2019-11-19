eac49515626f08251288bbf19f65e29ed24223b6
Ak543ito <dmarc.baensch@gmail.com>
2019-11-19 12:56:29 +0100

    update

---
+++ exercise 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+        
+            Console.WriteLine("Hello. Pls insert your name:     ");
+            name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + "!");
+        }
+    }
+}
---
+++ exercise 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            int number1 = 0;
+            int number2 = 0;
+            int result = 0;
+            
+            Console.WriteLine("Choose your operator:    ");
+            op = Console.ReadLine();
+            Console.WriteLine("Choose your first number:    ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose your second number:    ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if(op == "/" ){
+                result = number1 / number2;
+            }
+            if(op == "+" ){
+                result = number1 + number2;
+            }
+            if(op == "-" ){
+                result = number1 - number2;
+            }
+            if(op == "*" ){
+                result = number1 * number2;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
